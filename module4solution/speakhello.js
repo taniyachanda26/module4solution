91bbdc864ad55dc16ebb760b9f490a3338abc8c8
taniyachanda26 <64738232+taniyachanda26@users.noreply.github.com>
2020-07-28 11:04:32 +0530

    Delete speakhello.js

--- module4solution/speakhello.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-    var byespekar={};
-    var say=Good Bye;
-    byespekar.saybye=function(name){
-        console.log(say +" "+name);
-    }
-    window.byespekar=byespekar;
-})(window);
